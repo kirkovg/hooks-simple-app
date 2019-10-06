ab68a944b4a561aa35dccadddc47e89c4439b932
Gjorgji Kirkov <gjorgjikirkov@gmail.com>
2019-10-06 23:40:56 +0200

    [modern-react-course] Section 25 - finished section 25

---
+++ src/components/ResourceList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import useResources from './useResources';
+
+const ResourceList = ({resource}) => {
+  const resources = useResources(resource);
+
+  return (
+    <ul>
+      {resources.map(r => <li key={r.id}>{r.title}</li>)}
+    </ul>
+  );
+};
+
+export default ResourceList;
